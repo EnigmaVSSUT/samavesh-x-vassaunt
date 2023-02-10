9ea0db0ed8cfbffcebc92050b654652e737251a3
Princerey <rohkumar0126@gmail.com>
2023-02-10 15:27:37 +0530

    hydration error fixed

--- components/timer.jsx
+++ components/timer.jsx
@@ -1,6 +1,7 @@
 import { Stack, Typography,Box} from "@mui/material"
 import { Container } from "@mui/system";
 import Countdown,{zeroPad} from 'react-countdown';
+import { HydrationProvider, Server, Client } from "react-hydration-provider";
 const Completionist = () => <Typography sx={{fontSize:"100px",color:'#C0000A',fontWeight:'900'}}>The Fest is ON!</Typography>
 const renderer = ({ days,hours, minutes, seconds, completed }) => {
     if (completed) {
@@ -9,12 +10,13 @@ const renderer = ({ days,hours, minutes, seconds, completed }) => {
     } else {
       // Render a countdown
       return  <Typography sx={{fontSize:"100px",color:'#C0000A',fontWeight:'900',}}>
-      {zeroPad(days)}:{(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+      {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
       </Typography>;
     }
   };
 const Timer = () => {
 	return (
+    <HydrationProvider>
         <Stack
 			padding='10px 32px'
 			alignItems='center'sx={{background:"#F3EEE8"}}
@@ -32,14 +34,21 @@ alignItems: "flex-start"}}>
     }}
     >
         <Typography sx={{color:"#C0000A",fontWeight:"900",fontSize:"36px",lineHeight:"44px"}}>Starting in</Typography>
-        <Countdown
-    date= {1677868200000}
-    // date={Date.now()+5000}
+        <Server><Countdown
+    // date= {1677868200000}
+    date={Date.now()+50000}
     renderer={renderer}
-  />
+  /></Server>
+   <Client><Countdown
+    date= {1677868200000}
+    // date={Date.now()+50000}
+    renderer={renderer}
+  /></Client>
+
     </Container>
 </Box>
 </Stack>
+</HydrationProvider>
     )
 }
 export default Timer
