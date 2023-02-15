9b6111daaadbd873d75d1545861733d3745f9a1e
Soham Samantaray <ssoham414@gmail.com>
2023-02-16 00:09:54 +0530

    Club details page updated

--- pages/club/[club].jsx
+++ pages/club/[club].jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { useEffect } from "react";
 import ClubCard from "@/components/ClubCard";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Avatar } from "@mui/material";
 
 const ClubPage = () => {
   const router = useRouter();
@@ -25,6 +25,81 @@ const ClubPage = () => {
 
   return (
     <>
+      <Stack
+        sx={{
+          justifyContent: "space-between",
+          padding: "20px",
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          margin: { xs: "0 auto", md: "0px" },
+        }}
+      >
+        <Stack>
+          <Typography
+            variant="h2"
+            color="initial"
+            sx={{
+              textDecoration: "underline",
+              fontFamily: "BentonSans Comp Black",
+              textTransform: "uppercase",
+            }}
+          >
+            {club.name}
+          </Typography>
+          <Typography
+            variant="h6"
+            color="initial"
+            sx={{
+              fontFamily: "BentonSans Comp Black",
+              textTransform: "uppercase",
+              marginBottom: "18px",
+            }}
+          >
+            {club.about}
+          </Typography>
+        </Stack>
+        <Stack
+          alignItems={{
+            xs: "center",
+            md: "start",
+          }}
+        >
+          <Typography
+            variant="h4"
+            color="initial"
+            sx={{
+              marginBottom: "30px",
+              fontFamily: "BentonSans Comp Black",
+              textTransform: "uppercase",
+            }}
+          >
+            POINT OF CONTACT
+          </Typography>
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: { lg: "row", md: "row", sm: "coloumn" },
+              margin: { md: "0", sm: "0 auto" },
+            }}
+          >
+            {club.poc.map((e) => (
+              <Stack sx={{ alignItems: "center", marginRight: "10px" }}>
+                <Avatar>N</Avatar>
+                <Typography variant="subtitle1" color="initial">
+                  {e.name}
+                </Typography>
+                <Typography variant="caption" color="initial">
+                  {e.designation}
+                </Typography>
+                <Typography variant="caption" color="initial">
+                  {e.contactNo}
+                </Typography>
+              </Stack>
+            ))}
+          </Stack>
+        </Stack>
+      </Stack>
+
       <Stack
         sx={{ padding: "15px" }}
         alignItems={{ xs: "center", sm: "center", md: "start" }}
