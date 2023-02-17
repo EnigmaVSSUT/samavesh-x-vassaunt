ac5ec4d1c3a4653e37dc4d5b46c50652edd09593
Soham Samantaray <ssoham414@gmail.com>
2023-02-17 22:31:56 +0530

    Form validation updated

--- pages/registration.jsx
+++ pages/registration.jsx
@@ -59,8 +59,7 @@ const Form = () => {
     register,
     formState: { errors },
   } = useForm();
-  const regexExp =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+  const regexExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   const handleOpen = () => {
     console.log(
