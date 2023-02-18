29f9f634ce6300a5c3227cd4b6a70cc397195ead
asutosh-147 <98374996+asutosh-147@users.noreply.github.com>
2023-02-18 17:53:02 +0530

    profile

--- pages/account.jsx
+++ pages/account.jsx
@@ -11,40 +11,74 @@ const Account = () => {
       <Stack
         sx={{ paddingTop: "50px", margin: { md: "0 auto", sm: "0 auto" } }}
       >
-        <Stack
+        <Typography
+          variant="h2"
+          color="initial"
+          textAlign="center"
           sx={{
-            justifyContent: "space-between",
-            padding: "20px",
-            display: "flex",
-            flexDirection: { xs: "column", md: "row" },
-            margin: { xs: "0 auto", md: "0px" },
+            textDecoration: "underline",
+            fontFamily: "BentonSans Comp Black",
+            textTransform: "uppercase",
           }}
         >
-          <Stack>
-            <Typography
-              variant="h2"
-              color="initial"
-              sx={{
-                textDecoration: "underline",
-                fontFamily: "BentonSans Comp Black",
-                textTransform: "uppercase",
-              }}
-            >
-              My Profile
-            </Typography>
-            <Typography
-              variant="h6"
-              color="initial"
-              sx={{
-                fontFamily: "BentonSans Comp Black",
-                textTransform: "uppercase",
-                marginBottom: "18px",
-              }}
-            >
-              Events Participated
-            </Typography>
+          My Profile
+        </Typography>
+        <Stack
+          padding="60px 30px"
+          margin="3rem"
+          direction={{
+            xs: "column",
+            md: "row",
+          }}
+          alignItems="center"
+          justifyContent="space-between"
+          spacing={{
+            xs: 5,
+            md: 40,
+          }}
+          sx={{
+            backgroundColor: "#C0000A",
+            borderRadius: "20px",
+          }}
+        >
+          <Avatar
+            src="/clubs/5.png"
+            sx={{
+              width: {
+                xs: 175,
+                md: 250,
+              },
+              height: {
+                xs: 175,
+                md: 250,
+              },
+            }}
+          >
+            P
+          </Avatar>
+          <Stack
+            direction="column"
+            spacing={{ xs: 1, md: 3 }}
+            sx={{ color: "white" }}
+          >
+            <Typography variant="h5">Name : Andu Pandu</Typography>
+            <Typography variant="h5">Institution : VSSUT</Typography>
+            <Typography variant="h5">Branch : CSE</Typography>
+            <Typography variant="h5">Graduation Year : 2025</Typography>
           </Stack>
         </Stack>
+        <Typography
+          variant="h4"
+          textAlign="center"
+          color="initial"
+          sx={{
+            fontFamily: "BentonSans Comp Black",
+            textTransform: "uppercase",
+            marginBottom: "18px",
+          }}
+        >
+          Events Participated
+        </Typography>
       </Stack>
       <Events />
     </>
