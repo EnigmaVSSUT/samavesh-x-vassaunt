321d40f6873ad59906e9f0d351577d585b9f49a6
Princerey <rohkumar0126@gmail.com>
2023-02-19 05:14:09 +0530

    conditional rendering of generate ticket and layout changes

--- components/ticket.jsx
+++ components/ticket.jsx
@@ -6,14 +6,72 @@ import ReactToPrint from "react-to-print";
 import svlogo from "/pictures/svlogo.png";
 import vssutlogo from "/pictures/vssut_logo.png";
 import Image from "next/image";
+// import jsPDF from "jspdf";
+// import html2canvas from "html2canvas";
 
 const Ticket = () => {
+  // const handleDownload = useReactToPrint({
+  //   onPrintError: (error) => console.log(error),
+  //   content: () => ref.current,
+  //   removeAfterPrint: true,
+  //   print: async (printIframe) => {
+  //     const document = printIframe.contentDocument;
+  //     if (document) {
+  //       const html = document.getElementsByTagName("html")[0];
+  //       console.log(html);
+  //       const exporter = new Html2Pdf(html);
+  //       await exporter.getPdf(true);
+  //     }
+  //   },
+  // });
+
+  const [image, setImage] = useState(null);
+  //   const handleGeneratePdf1 = () => {
+  //   html2canvas(ref.current,{backgroundColor:null}).then(function (canvas) {
+  //     var img = canvas.toDataURL("image/png");
+  //     var doc = new jsPDF({
+  //       orientation: "landscape",
+  //     });
+  //     doc.addImage(img, 'PNG', 0, 0,298,210);
+  //     doc.save('Ticket.pdf');
+  // });
+  //   }
+
   const [file, setFile] = useState();
   function handleChange(e) {
-    console.log(e.target.files);
+    setImage(e.target.files[0]);
+    // console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
   const ref = useRef();
+
+  if (image) {
+    var shouldPrint = true;
+  } else {
+    shouldPrint = false;
+  }
+  // const handleGeneratePdf = () => {
+  // 	const doc = new jsPDF({
+  //     orientation: "landscape",
+  //   });
+
+  // 	// Adding the fonts.
+  // 	doc.setFont('Product sans');
+
+  // 	doc.html(ref.current, {
+  // 		async callback(doc) {
+  // 		 doc.save('Ticket');
+  // 		},
+  //     // margin: [10, 10, 10, 10],
+  //       autoPaging: 'text',
+  //       x: -14,
+  //       y: 7,
+  //       width: 325, //target width in the PDF document
+  //       windowWidth: 540,
+  //       windowHeight:200
+
+  // 	});
+  // };
   return (
     <>
       <Box
@@ -22,8 +80,8 @@ const Ticket = () => {
         borderRadius="10px"
         margin="0 auto"
         padding="15px"
-        paddingLeft="40px"
-        paddingRight="40px"
+        paddingLeft={{ xs: "10px", sm: "40px" }}
+        paddingRight={{ xs: "10px", sm: "40px" }}
         ref={ref}
         sx={{
           backgroundImage: `url(${svlogo.src})`,
@@ -56,21 +114,40 @@ const Ticket = () => {
         <Stack direction="row" alignItems="center" spacing={1}>
           <Box
             sx={{
-              margin: "10px",
-              height: 125,
-              width: 125,
-              borderRadius: "50%",
-              border: "1px solid #c0c0c0",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <img
-              src={file}
-              height="125px"
-              width="125px"
-              style={{ borderRadius: "50%" }}
-            />
+            <Typography
+              sx={{
+                position: "relative",
+                top: "85px",
+                opacity: "0.8",
+                color: "#ead6d6bf",
+                // fontSize:"1.2rem"
+              }}
+            >
+              #844
+            </Typography>
+            <Box
+              sx={{
+                margin: "10px",
+                height: 125,
+                width: 125,
+                borderRadius: "50%",
+                border: "1px solid #c0c0c0",
+              }}
+            >
+              <img
+                src={file}
+                height="125px"
+                width="125px"
+                style={{ borderRadius: "50%" }}
+              />
+            </Box>
           </Box>
-
           <Stack
             direction="column"
             justifyContent="space-between"
@@ -83,7 +160,17 @@ const Ticket = () => {
               display="flex"
               alignItems="center"
             >
-              <Typography sx={{ color: "white" }}>NAME:</Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  display: {
+                    xs: "none",
+                    sm: "block",
+                  },
+                }}
+              >
+                NAME:
+              </Typography>
               <TextField
                 id="name"
                 size="small"
@@ -103,7 +190,17 @@ const Ticket = () => {
               display="flex"
               alignItems="center"
             >
-              <Typography sx={{ color: "white" }}>COLLEGE:</Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  display: {
+                    xs: "none",
+                    sm: "block",
+                  },
+                }}
+              >
+                COLLEGE:
+              </Typography>
               <TextField
                 id="college"
                 size="small"
@@ -123,7 +220,17 @@ const Ticket = () => {
               display="flex"
               alignItems="center"
             >
-              <Typography sx={{ color: "white" }}>Branch:</Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  display: {
+                    xs: "none",
+                    sm: "block",
+                  },
+                }}
+              >
+                Branch:
+              </Typography>
               <TextField
                 id="branch"
                 size="small"
@@ -143,7 +250,17 @@ const Ticket = () => {
               display="flex"
               alignItems="center"
             >
-              <Typography sx={{ color: "white" }}>Year:</Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  display: {
+                    xs: "none",
+                    sm: "block",
+                  },
+                }}
+              >
+                Year:
+              </Typography>
               <TextField
                 id="year"
                 size="small"
@@ -173,12 +290,22 @@ const Ticket = () => {
       >
         <Button variant="contained" component="label">
           Upload Image
-          <input type="file" accept="image/*" onChange={handleChange} hidden />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleChange}
+            hidden
+            required
+          />
         </Button>
-        <ReactToPrint
-          trigger={() => <Button variant="contained">Generate & Print</Button>}
-          content={() => ref.current}
-        />
+        {shouldPrint && (
+          <ReactToPrint
+            trigger={() => (
+              <Button variant="contained">Generate & Print</Button>
+            )}
+            content={() => ref.current}
+          />
+        )}
       </Box>
     </>
   );
